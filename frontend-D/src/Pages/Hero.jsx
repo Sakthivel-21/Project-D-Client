9ef4055e217ba71d98f9021c94aaaa419e9c2ffd
sakthi <sakthivelkalidass@gmail.com>
2025-04-06 13:40:04 +0530

    hero add

--- frontend-D/src/Pages/Hero.jsx
+++ frontend-D/src/Pages/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
             <SwiperSlide key={index}>
               <img src={src} alt={`Slide ${index + 1}`} className="w-full   h-[500px]" />
               <div className="absolute inset-0  items-center justify-center bg-black bg-opacity-20 text-center ">
-                <h1 className="text-white text-2xl font-bold mt-16">Welcome to Dindigul City ! ➕</h1>
+                <h1 className="text-white text-2xl font-bold mt-16">Welcome to Dindigul City ! </h1>
                 <p className="text-white text-lg font-bold sm:w-[600px] sm:ml-[465px] mt-12 pl-4 pr-4">
                 The ultimate directory of workplaces and services in Dindigul district! Whether you're looking for colleges, industries, skilled professionals, repair services, or local businesses, we provide a comprehensive database to help you find the right place with ease.
 
